745baaf80ce71e8794dab19f64502aad2bb95362
NgocAnMaster <lengocanthyh@gmail.com>
2026-04-01 07:51:40 +0700

    update ShowGird

--- frontend/src/components/ShowGrid.jsx
+++ frontend/src/components/ShowGrid.jsx
@@ -2,7 +2,7 @@ import ShowCard from "./ShowCard";
 
 export default function ShowGrid({ shows }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {shows.map((s) => (
         <ShowCard key={s._id} show={s} />
       ))}
